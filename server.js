e23a06c4334f7693cacf9fba6eebb7839424824c
mohamedveron <mohamedveron23@gmail.com>
2020-11-07 03:34:46 +0200

    connect to snowflake

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+var snowflake = require('snowflake-sdk')
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -8,6 +10,17 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello World');
 });
+
+// Create a Connection object that we can use later to connect.
+var connection = snowflake.createConnection( {
+    account: 'paulig.west-europe.azure',
+    username: 'dev_edw_junction_team_05',
+    password: 'pnimAo2Dd3swMaq2dVJNBa5qJx7LJUhr',
+    database: 'DEV_EDW_JUNCTION'
+    }
+    );
+
+    console.log(connection)
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
